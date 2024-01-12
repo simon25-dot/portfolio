0d798e2564d7936179465ce5e58c206f5f9950f8
simon25-dot <134793715+simon25-dot@users.noreply.github.com>
2024-01-12 19:51:10 +0300

    Add files via upload

---
+++ vigon.js
@@ -0,0 +1,18 @@
+//dark mode toogle
+let darkModeIcon = document.querySelector("#moon")
+darkModeIcon.onclick = () =>{
+          darkModeIcon.classList.toggle(<i class="fa-regular fa-sun"></i>)
+}
+
+
+//ScrollReveal({ 
+          //reset: true,
+          //distance:'80px',
+          //duration:2000,
+          //delay:200
+//});
+
+//ScrollReveal().reveal('.head, .home', { origin:(top) });
+
+
+      
